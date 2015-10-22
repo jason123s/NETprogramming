bec473e65849063a83038a9a08f84c76e5632d24
jason123s <hnguyen1193s@gmail.com>
2015-10-21 21:01:12 -0400

    First Initalization of Mini Minecraft Game

---
+++ MiniMineCraft/MiniMineCraft/MiniMineCraft/ScreenCamera.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MiniMineCraft
+{
+    class ScreenCamera
+    {
+        public Matrix transform;
+        Viewport view;
+        Vector2 centre;
+
+        public ScreenCamera(Viewport view)
+        {
+            this.view = view; 
+        }
+
+        public void Update(GameTime gameTime, Game1 ship)
+        {
+            centre = new Vector2(500.0f, 500.0f);
+            transform = Matrix.CreateScale(new Vector3(1, 1, 0)) * Matrix.CreateTranslation(new Vector3(-centre.X, -centre.Y,0));
+        }
+    }
+}
